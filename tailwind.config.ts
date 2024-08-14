935711bba0beff43f0bbbfcb922eceaa59c3608d
Your Name <waltheo319@gmail.com>
2024-08-14 13:31:32 -0700

    styles, tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,40 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        xxs: "350px",
+        xs: "480px",
+        sm: "640px",
+        md: "840px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+        "3xl": "1620px",
+        "4xl": "1920px",
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-radial":
+          "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        sacramento: ["var(--font-sacramento)"],
+        roboto: ["var(--font-roboto)"],
+      },
+      boxShadow: {
+        "inset-shadow": "inset 0 -3em 3em rgb(0 200 0 / 30%)",
+        "primary-color-sm":
+          "1px 2px 2px 1px rgba(238, 139, 172, 0.5)",
+        "primary-color-md":
+          "1px 3px 4px 1px rgba(238, 139, 172, 0.5)",
+      },
+      colors: {
+        primary: "rgb(218, 87, 150)",
+        secondary: "antiquewhite",
+      },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
       },
     },
   },
